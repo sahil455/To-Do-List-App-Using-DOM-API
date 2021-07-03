16406bb0a6957ea6ab6520cc1647e3a3b4eaa9c8
Sahil Jain <42314817+sahil455@users.noreply.github.com>
2021-07-03 23:08:05 +0530

    Mini Web App

---
+++ To-Do list App/app.js
@@ -0,0 +1,82 @@
+// let books= document.querySelectorAll("#book-list li .name");
+// Array.from(books).forEach((books)=>{
+//     books.textContent +="hello"
+// })
+
+// var book=document.querySelector("#book-list");
+// //book.innerHTML ='<h2>Book more to add</h2>'
+// book.innerHTML +='<h2>Book more to add</h2>'
+
+// var blist=document.querySelector("#book-list")
+// console.log(blist.children)
+// console.log(blist.nextElementSibling);
+// blist.previousElementSibling.querySelector('p').innerHTML='<br> yo yo'
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+    //Delete the Activity
+
+const list =document.querySelector('#item-list ul')
+
+list.addEventListener('click',(e)=>{
+    if(e.target.className=='delete'){
+    const li1=e.target.parentElement
+    list.removeChild(li1)
+    }
+})
+
+// add Activity
+
+var form=document.forms['add-item']
+form.addEventListener('submit',(e)=>{
+e.preventDefault()
+const value=form.querySelector('input[type="text"]').value;
+
+//create elements
+const li2=document.createElement('li')
+const title=document.createElement('span')
+const deletebtn=document.createElement('span')
+
+//textcontent
+
+deletebtn.textContent='delete'
+title.textContent=value;
+
+//add classes
+
+title.classList.add('name')
+deletebtn.classList.add('delete')
+
+//append elememts
+
+li2.appendChild(title)
+li2.appendChild(deletebtn)
+list.appendChild(li2)
+
+})
+
+//hide Activites
+
+const hide=document.querySelector('#hide')
+hide.addEventListener('change',(e)=>{
+if(hide.checked)
+    list.style.display="none";
+else
+    list.style.display="initial";
+})
+
+//search Items
+
+var searchBox=document.forms['search-items'].querySelector('input')
+searchBox.addEventListener('keyup',(e)=>{
+const text=e.target.value.toLowerCase()
+var items=list.getElementsByTagName('li')
+Array.from(items).forEach((item)=>{
+    var activity=item.firstElementChild.textContent
+    if(activity.toLowerCase().indexOf(text)!=-1)
+        item.style.display='block';
+    else
+        item.style.display='none';
+})
+})
+})
